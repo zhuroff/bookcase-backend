56b38e59283e0d193ef8e571d501f5e5c0cabfb3
zhuroff <ezhuroff@gmail.com>
2022-01-08 01:00:56 +0300

    single list data fix

--- controllers/list.controller.ts
+++ controllers/list.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { getCategories } from '../shared/caterories-getters'
 import List from '../models/list.model'
+// import Author from '../models/author.model'
 
 const listsCollections = async (req: Request, res: Response) => {
   try {
@@ -27,6 +28,19 @@ const listShort = async (req: Request, res: Response) => {
 const listFull = async (req: Request, res: Response) => {
   try {
     const response = await List.findById(req.params.id)
+      .populate({
+        path: 'lists.contents.book',
+        select: [
+          '_id',
+          'title',
+          'subtitle',
+          'coverImage',
+          'status'
+        ],
+        populate: [
+          { path: 'authors.author', select: ['title', '_id'] }
+        ]
+      })
     res.json(response)
   } catch (error) {
     res.status(500).json(error)
